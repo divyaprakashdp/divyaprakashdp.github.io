f51377e4f160dbb91ca048043eee24f048ff5e83
divyaprakash <d.prakash1506@gmail.com>
2024-01-05 00:59:13 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import Navbar from "./Navbar";
+
 export default function App() {
-  return <h1 className="text-4xl underline font-heading">This is a heading</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <div>
+      <h2>dp</h2>
+      <div>
+        <ul>
+          <li>About</li>
+          <li>Skill</li>
+          <li>Project</li>
+          <li>Experience</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
