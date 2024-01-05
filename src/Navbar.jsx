b7517e6d86ba86a8ed9ae4a01af10dda33b1c84f
divyaprakash <d.prakash1506@gmail.com>
2024-01-05 10:44:16 +0530

    updated navbar functionality

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,41 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
 export default function Navbar() {
+  const [navBtn, setNavBtn] = useState(false);
+  const navLinks = [
+    { id: 1, link: "About" },
+    { id: 2, link: "Skills" },
+    { id: 3, link: "Projects" },
+    { id: 4, link: "Experience" },
+    { id: 5, link: "Contact" },
+  ];
+
+  const navList = navLinks.map(({ id, link }) => (
+    <li
+      key={id}
+      className="p-4 cursor-pointer uppercase font-medium text-xl text-gray-500 hover:scale-110 duration-200"
+    >
+      {link}
+    </li>
+  ));
+
   return (
-    <div>
-      <h2>dp</h2>
-      <div>
-        <ul>
-          <li>About</li>
-          <li>Skill</li>
-          <li>Project</li>
-          <li>Experience</li>
-          <li>Contact</li>
-        </ul>
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+      <h2 className=" font-logo text-5xl ml-4">dp</h2>
+
+      <ul className="hidden md:flex">{navList}</ul>
+      <div
+        className="md:hidden cursor-pointer text-gray-500 pr-4 z-10"
+        onClick={() => setNavBtn(!navBtn)}
+      >
+        {navBtn ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
+      {navBtn && (
+        <ul className="flex flex-col justify-center items-center absolute top-12 right-0 w-18 h-18 bg-gradient-to-b from-black to-gray-800">
+          {navList}
+        </ul>
+      )}
     </div>
   );
 }
