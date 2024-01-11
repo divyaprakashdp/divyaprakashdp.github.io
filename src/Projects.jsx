6cf602efb89cc495937fb3e1433cd850ccbd9aad
divyaprakash <d.prakash1506@gmail.com>
2024-01-11 22:47:48 +0530

    Added projects section

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,3 +1,7 @@
+import expenseTrackerImg from "./assets/expenseTracker.png";
+import polarixImg from "./assets/Polarix.png";
+import timemachine from "./assets/timemachine.png";
+
 export default function Projects() {
   const techStack = [
     {
@@ -6,37 +10,46 @@ export default function Projects() {
       title: "BOOKQUEST",
       style: "shadow-red-900",
       link: "https://github.com/divyaprakashdp/bookquest",
+      imgSrc: expenseTrackerImg,
     },
     {
       id: 2,
       icon: <br size={80} className="text-blue-500" />,
       title: "TIME-MACHINE",
-      style: "shadow-blue-900",
+      style: "shadow-[#AAC9AF]",
       link: "https://github.com/divyaprakashdp/time-machine",
+      imgSrc: timemachine,
     },
     {
       id: 3,
       icon: <br size={80} className="text-yellow-500" />,
       title: "EXPENSE-TRACKER",
-      style: "shadow-yellow-900",
+      style: "shadow-[#8D60FB]",
       link: "https://github.com/divyaprakashdp/expense-tracker-app",
+      imgSrc: expenseTrackerImg,
     },
     {
       id: 4,
       icon: <br size={80} className="text-blue-500" />,
       title: "POLARIX",
-      style: "shadow-blue-800",
+      style: "shadow-[#C8434A]",
       link: "",
+      imgSrc: polarixImg,
     },
   ];
 
-  const techStackDiv = techStack.map(({ id, icon, title, style, link }) => (
+  const techStackDiv = techStack.map(({ id, title, style, link, imgSrc }) => (
     <div
       key={id}
       className={`py-2 shadow-md text-center justify-center hover:scale-110 duration-500 rounded-xl hover:rounded-lg ${style}`}
     >
-      <p className="mt-2">{title}</p>
-      <div className="w-20 mx-auto ">{icon}</div>
+      <div>
+        <p className="mt-2">{title}</p>
+      </div>
+
+      <div className="w-100 mx-4 my-4 ">
+        <img src={imgSrc} className="rounded:lg" />
+      </div>
       <div className="text-sm">
         <a
           href={link}
@@ -60,7 +73,7 @@ export default function Projects() {
   return (
     <div
       id="Projects"
-      className="md:h-screen w-full text-xl uppercase bg-gradient-to-b from-gray-800 via-black to-gray-800 text-white"
+      className="w-full text-xl uppercase bg-gradient-to-b from-gray-800 via-black to-gray-800 text-white"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
